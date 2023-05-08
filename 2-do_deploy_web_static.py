ecc36f4c8bb8c3583c5884384db761f7d60df6ab
Cynthia Uche <cynthianuchepro@gmail.com>
2023-05-07 21:13:45 -0700

    Deploy web static

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This module contians a function do_deploy."""
+from fabric.api import *
+import os.path
+
+
+env.user = "ubuntu"
+env.hosts = ["52.86.108.58", "100.25.205.85"]
+
+def do_deploy(archive_path):
+    """Distributes an archive to web servers"""
+    if not os.path.exists(archive_path):
+        return False
+    try:
+        put(archive_path, "/tmp/")
+        filename_with_ext = archive_path.split('/')[-1]
+        filename_no_ext = filename_with_ext.split('.')[0]
+        run("mkdir -p /data/web_static/releases/{}".format(filename_no_ext))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(filename_with_ext, filename_no_ext))
+        run("rm /tmp/{}".format(filename_with_ext))
+        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}".format(filename_no_ext, filename_no_ext))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(filename_no_ext))
+        return True
+    except BaseException:
+        return False
