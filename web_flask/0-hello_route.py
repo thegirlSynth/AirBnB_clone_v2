0af040f6256ab3c33be01bca505e3b75d42f1ec0
Cynthia Uche <cynthianuchepro@gmail.com>
2023-05-21 18:30:50 -0700

    Fix PEP style

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,10 +6,12 @@ from flask import Flask
 
 webapp = Flask(__name__)
 
+
 @webapp.route('/', strict_slashes=False)
 def say_hello():
     """Displays a message."""
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     webapp.run(host='0.0.0.0', port=5000)
