ff82f7c37f9d3a338103d776cacd09d0de7b8d6e
Cynthia Uche <cynthianuchepro@gmail.com>
2023-05-21 18:11:40 -0700

    Start a Flask web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This script starts a Flask web application."""
+
+from flask import Flask
+
+
+webapp = Flask(__name__)
+
+@webapp.route('/', strict_slashes=False)
+def say_hello():
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    webapp.run(host='0.0.0.0', port=5000)
