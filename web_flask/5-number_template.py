8d79d0d6867fddb985e99dab036148db15366105
Cynthia Uche <cynthianuchepro@gmail.com>
2023-05-25 05:31:49 -0700

    Create 5-number_template.py

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""This script starts a Flask web application."""
+
+from flask import Flask, render_template
+
+
+webapp = Flask(__name__)
+
+
+@webapp.route('/', strict_slashes=False)
+def say_hello():
+    return 'Hello HBNB!'
+
+
+@webapp.route('/hbnb', strict_slashes=False)
+def say_HBNB():
+    return 'HBNB'
+
+
+@webapp.route('/c/<text>', strict_slashes=False)
+def replace_C(text):
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+@webapp.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@webapp.route('/python/<text>', strict_slashes=False)
+def replace_Python(text):
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+@webapp.route('/number/<int:n>', strict_slashes=False)
+def show_number(n):
+    return '{} is a number'.format(n)
+
+
+@webapp.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n=None):
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    webapp.run(host='0.0.0.0', port=5000)
